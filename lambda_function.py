8feb54b9f6e2c1a01ab74c819bb026a561900ae2
vesono <rbrb-sry229@outlook.com>
2020-01-09 15:24:07 +0900

    photo save to s3

--- lambda_function.py
+++ lambda_function.py
@@ -2,14 +2,16 @@
 # ライブラリ読みこみ 
 import os
 import datetime
+from io import BytesIO
 import boto3
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from linebot.models import MessageEvent, TextMessage, TextSendMessage, ImageMessage
 
 # s3設定
-s3 = boto3.resource('s3')
-bucket = s3.Bucket('photo-recog-line-bot')
+s3r = boto3.resource('s3')
+bucket = s3r.Bucket('photo-recog-line-bot')
+s3c = boto3.client('s3')
 
 # LineBotApiインスタンスの作成
 # CHANNEL_ACCESS_TOKEN: 各API通信を行うときに使用
@@ -19,32 +21,49 @@ channel_access_token = os.getenv('CHANNEL_ACCESS_TOKEN', None)
 line_bot_api = LineBotApi(channel_access_token)
 handler = WebhookHandler(channel_secret)
 
+# ファイル名用日時取得関数
+def now_time():
+    now = datetime.datetime.today()
+    return now.strftime("%Y%m%d%H%M%S") + str(now.microsecond)
+
 def lambda_handler(event, context):
     ### イベントごとに関数を定義
+    # テキストメッセージの場合、オウム返しする
     @handler.add(MessageEvent, message=TextMessage)
     def handle_message(line_event):
-        # テキストメッセージの場合、オウム返しする
         line_bot_api.reply_message(
             line_event.reply_token,
             TextSendMessage(text=line_event.message.text))
-        # s3保存処理(tmpにファイル作成 ⇒ s3に保存)
-        now = datetime.datetime.today()
-        name_tmpfile = now.strftime("%Y%m%d%H%M%S") + str(now.microsecond) + '_lb'
-        path_tmpfile = '/tmp/' + name_tmpfile + '.txt'
-        tmpfile = open(path_tmpfile,'w')
-        tmpfile.write(line_event.message.text)
-        tmpfile.close()
-        bucket.upload_file(path_tmpfile, 'test/' + name_tmpfile + '.txt')
-        # tmpファイル削除
-        os.remove(path_tmpfile)
+        # # s3保存処理(tmpにファイル作成 ⇒ s3に保存)
+        # now = now_time()
+        # name_tmpfile = now + '_lbot_text'
+        # path_tmpfile = '/tmp/' + name_tmpfile + '.txt'
+        # tmpfile = open(path_tmpfile,'w')
+        # tmpfile.write(line_event.message.text)
+        # tmpfile.close()
+        # bucket.upload_file(path_tmpfile, 'test/' + name_tmpfile + '.txt')
+        # # tmpファイル削除
+        # os.remove(path_tmpfile)
+
+    # 画像メッセージの場合は画像をs3に保存
+    @handler.add(MessageEvent, message=ImageMessage)
+    def handle_image(line_event):
+        # 画像データを取得
+        message_id = line_event.message.id
+        message_content = line_bot_api.get_message_content(message_id)
+        image_bin = BytesIO(message_content.content)
+        image = image_bin.getvalue()
+        # S3へ画像を保存
+        now = now_time()
+        s3_filepath = 'pic/' + now + '_lbot_image' + '.jpg'
+        s3c.put_object(Bucket='photo-recog-line-bot', Body=image,  Key=s3_filepath)
 
     # debug
-    #print(event)
+    print(event)
 
-    # レスポンスの関数呼び出しとリクエストの署名検証
+    ### レスポンスの関数呼び出しとリクエストの署名検証
     # get X-Line-Signature header value
     signature = event["headers"]["X-Line-Signature"]
-
     # get request body
     body = event["body"]
 
