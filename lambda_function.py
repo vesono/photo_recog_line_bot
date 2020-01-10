c6e8841eea9a6401a3f9c22d65bef94ab4dad01c
vesono <rbrb-sry229@outlook.com>
2020-01-10 17:21:23 +0900

    modify reply,log

--- lambda_function.py
+++ lambda_function.py
@@ -33,14 +33,36 @@ def rtn_string(rp_key, detect_res):
     for item in detect_res[rp_key]:
         if type(detect_res[rp_key]) == dict:
             values = detect_res[rp_key][item]
-            k_value = round(values, 2) if type(values)==float else values
-            rtn_str = rtn_str + '\n' + '  ' + item + '：' + str(k_value)
+            k_value = str(round(values, 2)) + '%' if type(values)==float else str(values)
+            rtn_str = rtn_str + '\n' + '      ' + item + '：' + k_value
         else:
             for keys in item:
                 values = item[keys]
-                k_value = round(values, 2) if type(values)==float else values
-                rtn_str = rtn_str + '\n' + '  ' + keys + '：' + str(k_value)
+                k_value = str(round(values, 2)) + '%' if type(values)==float else str(values)
+                rtn_str = rtn_str + '\n' + '      ' + keys + '：' + k_value
     return rtn_str
+
+# 日本語変換
+def rep_jpn(text):
+    jpn_txt = text
+    jpn_txt = jpn_txt.replace('Value：', '')
+    jpn_txt = jpn_txt.replace('Low', '')
+    jpn_txt = jpn_txt.replace('Gender', '・性別予想')
+    jpn_txt = jpn_txt.replace('Female', '女性')
+    jpn_txt = jpn_txt.replace('male', '・男性')
+    jpn_txt = jpn_txt.replace('AgeRange', '・年齢予想')
+    jpn_txt = jpn_txt.replace('Smile', '・笑顔かどうか')
+    jpn_txt = jpn_txt.replace('Emotions', '・感情')
+    jpn_txt = jpn_txt.replace('Type：CONFUSED', '困惑')
+    jpn_txt = jpn_txt.replace('Type：ANGRY', '怒り')
+    jpn_txt = jpn_txt.replace('Type：FEAR', '恐怖')
+    jpn_txt = jpn_txt.replace('Type：SURPRISED', '驚き')
+    jpn_txt = jpn_txt.replace('Type：HAPPY', '喜び')
+    jpn_txt = jpn_txt.replace('Type：SAD', '悲しみ')
+    jpn_txt = jpn_txt.replace('Type：CALM', '穏やか')
+    jpn_txt = jpn_txt.replace('Type：DISGUSTED', 'いらいら')
+    jpn_txt = jpn_txt.replace('Confidence', '')
+    return jpn_txt
 
 def lambda_handler(event, context):
     ### イベントごとに関数を定義
@@ -63,7 +85,6 @@ def lambda_handler(event, context):
         now = now_time()
         s3_filepath = 'pic/' + now + '_lbot_image' + '.jpg'
         s3c.put_object(Bucket='photo-recog-line-bot', Body=image, Key=s3_filepath)
-
         # rekognitionで画像認識
         response = rekognition.detect_faces(
                 Image={
@@ -76,25 +97,25 @@ def lambda_handler(event, context):
                     'ALL',
                 ]
             )
-
-        # CloudWatch出力用
-        print(s3_filepath)
-        print(response)
-
         # リプライメッセージ作成
         rtn_dict = response['FaceDetails'][0]
+        # CloudWatch出力
+        print(s3_filepath)
+        print(rtn_dict)
         get_keys = ['Gender', 'AgeRange', 'Smile', 'Emotions']
         rtn_text = {}
         for g_key in get_keys:
             rtn_text[g_key] = rtn_dict[g_key]
-        # rtn_json = json.dumps(rtn_text, indent=2)
+        rtn_text['AgeRange']['Low'] = int((rtn_text['AgeRange']['Low'] + rtn_text['AgeRange']['High']) / 2)
+        del rtn_text['AgeRange']['High']
         rekog_return = rtn_string('Gender', rtn_text) + '\n' + rtn_string('AgeRange', rtn_text) + '\n' + \
                        rtn_string('Smile', rtn_text) + '\n' + rtn_string('Emotions', rtn_text)
-
+        rekog_return_jpn = rep_jpn(rekog_return)
         # リプライ処理
         line_bot_api.reply_message(
             line_event.reply_token,
-            TextSendMessage(text=rekog_return))
+            TextSendMessage(text=rekog_return_jpn))
+
 
     ### レスポンスの関数呼び出しとリクエストの署名検証
     # get X-Line-Signature header value
